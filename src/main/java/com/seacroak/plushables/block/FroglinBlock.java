5bd24b338d19dc53e1c1bb47fe265bef8c1cc0e5
June <khazoda@gmail.com>
2022-06-21 23:24:04 +0100

    + shift right click pickup functionality

--- src/main/java/com/seacroak/plushables/block/FroglinBlock.java
+++ src/main/java/com/seacroak/plushables/block/FroglinBlock.java
@@ -1,18 +1,64 @@
 package com.seacroak.plushables.block;
 
+import com.seacroak.plushables.registry.SoundRegistry;
 import com.seacroak.plushables.util.VoxelShapeUtils;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ItemScatterer;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class FroglinBlock extends SimplePlushable {
 	public FroglinBlock() {
 		super();
+	}
+
+	// Shift Right Click pickup code
+	@Override
+	public ActionResult onUse(BlockState state, World world, BlockPos pos,
+			PlayerEntity player, Hand hand, BlockHitResult hit) {
+
+		// Serverside code
+		if (!world.isClient) {
+			if (player.isSneaking()) {
+				ItemScatterer.spawn(world, pos, DefaultedList.ofSize(1, new ItemStack(this)));
+				world.updateComparators(pos, this);
+				world.removeBlock(pos, false);
+				return ActionResult.CONSUME;
+			}
+		}
+		if (world.isClient) {
+			if (player.isSneaking()) {
+				world.playSound(player, pos, SoundRegistry.PLUSHABLE_POP, SoundCategory.BLOCKS, 0.5f, 1f);
+				world.playSound(player, pos, SoundEvents.BLOCK_WOOL_HIT, SoundCategory.BLOCKS, 0.5f, 1f);
+
+				// Custom breaking particle code
+				for (int i = 0; i < 5; i++) {
+					world.addParticle(ParticleTypes.POOF, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
+							rand.nextFloat(-0.05f, 0.05f), rand.nextFloat(-0.05f, 0.05f),
+							rand.nextFloat(-0.05f, 0.05f));
+					world.addParticle(ParticleTypes.GLOW, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
+							rand.nextFloat(-0.05f, 0.05f), rand.nextFloat(-0.05f, 0.05f),
+							rand.nextFloat(-0.05f, 0.05f));
+				}
+			}
+
+		}
+		return ActionResult.PASS;
 	}
 
 	// Bounding / Collision Box code
--- src/main/java/com/seacroak/plushables/registry/SoundRegistry.java
+++ src/main/java/com/seacroak/plushables/registry/SoundRegistry.java
@@ -9,4 +9,6 @@ import net.minecraft.util.registry.Registry;
 public class SoundRegistry {
 	public static SoundEvent BUILDER_DING = Registry.register(Registry.SOUND_EVENT, "builder_ding",
 			new SoundEvent(new Identifier(PlushablesMod.ModID, "builder_ding")));
+	public static SoundEvent PLUSHABLE_POP = Registry.register(Registry.SOUND_EVENT, "plushable_pop",
+			new SoundEvent(new Identifier(PlushablesMod.ModID, "plushable_pop")));
 }
