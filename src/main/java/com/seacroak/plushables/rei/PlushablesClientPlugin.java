fba61d0e589d45f460f8df6dd336d48ff00658c3
June <khazoda@gmail.com>
2022-06-22 15:08:18 +0100

    dependency, README updates

--- src/main/java/com/seacroak/plushables/rei/PlushablesClientPlugin.java
+++
@@ -1,6 +0,0 @@
-package com.seacroak.plushables.rei;
-
-import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
-
-public class PlushablesClientPlugin implements REIClientPlugin {
-}
--- src/main/java/com/seacroak/plushables/rei/PlushablesServerPlugin.java
+++
@@ -1,6 +0,0 @@
-package com.seacroak.plushables.rei;
-
-import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
-
-public class PlushablesServerPlugin implements REIServerPlugin {
-}
