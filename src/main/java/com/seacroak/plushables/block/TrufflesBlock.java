b0d856babd4b584862a38a5136c93a5bbae78902
June <khazoda@gmail.com>
2022-10-28 15:13:50 +0100

    Truffles model revision

--- src/main/java/com/seacroak/plushables/block/TrufflesBlock.java
+++ src/main/java/com/seacroak/plushables/block/TrufflesBlock.java
@@ -73,16 +73,17 @@ public class TrufflesBlock extends SimplePlushable {
 	// NORTH (Default) facing VoxelShape
 	static public VoxelShape getShape() {
 		VoxelShape shape = VoxelShapes.empty();
-		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.1875, 0, 0.125, 0.8125, 0.5, 0.75));
-		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.1875, 0, 0.0625, 0.25, 0.0625, 0.125));
-		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.75, 0, 0.0625, 0.8125, 0.0625, 0.125));
-		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.75, 0, 0.75, 0.8125, 0.0625, 0.875));
-		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.1875, 0.5, 0.125, 0.3125, 0.625, 0.125));
-		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.375, 0.125, 0.0625, 0.625, 0.25, 0.125));
-		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.6875, 0.5, 0.125, 0.8125, 0.625, 0.125));
-		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.4375, 0.25, 0.875, 0.5625, 0.375, 0.9375));
-		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.25, 0, 0.75, 0.75, 0.4375, 0.875));
-		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.1875, 0, 0.75, 0.25, 0.0625, 0.875));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.1875, 0, 0.25, 0.8125, 0.5, 0.75));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.25, 0, 0.0625, 0.3125, 0.0625, 0.125));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.6875, 0, 0.0625, 0.75, 0.0625, 0.125));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.25, 0.46875, 0.125, 0.375, 0.59375, 0.125));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.390625, 0.140625, 0.078125, 0.609375, 0.234375, 0.125));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.625, 0.46875, 0.125, 0.75, 0.59375, 0.125));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.4375, 0.25, 0.875, 0.5625, 0.375, 0.90625));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.21875, 0, 0.75, 0.78125, 0.46875, 0.875));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.71875, 0, 0.875, 0.78125, 0.0625, 0.9375));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.21875, 0, 0.875, 0.28125, 0.0625, 0.9375));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.21875, 0, 0.125, 0.78125, 0.46875, 0.25));
 
 		return shape;
 	}
