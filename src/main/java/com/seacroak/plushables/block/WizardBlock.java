7862cc215a69bd48d70709e535f5263efe1eb32d
Khazoda <khazoda@gmail.com>
2023-08-03 15:22:22 +0100

    Wizard plushable cooldown sync working mostly

--- src/main/java/com/seacroak/plushables/block/WizardBlock.java
+++ src/main/java/com/seacroak/plushables/block/WizardBlock.java
@@ -4,10 +4,13 @@ import com.seacroak.plushables.registry.SoundRegistry;
 import com.seacroak.plushables.util.networking.ParticlePacketHandler;
 import com.seacroak.plushables.util.networking.PlushablesNetworking;
 import com.seacroak.plushables.util.networking.SoundPacketHandler;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -17,11 +20,37 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.World;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class WizardBlock extends SimplePlushable {
+  public static final BooleanProperty ON_COOLDOWN = BooleanProperty.of("on_cooldown");
+  Timer timer = null;
+  TimerTask timerTask = null;
 
   public WizardBlock() {
     super();
+    setDefaultState(this.getStateManager().getDefaultState().with(ON_COOLDOWN, false));
   }
+
+  public Timer registerTimer(World world, BlockPos pos, BlockState state) {
+    Timer timer = new Timer();
+    this.timerTask = new TimerTask() {
+      @Override
+      public void run() {
+        world.setBlockState(pos, state.with(ON_COOLDOWN, false));
+        destroyTimer();
+      }
+    };
+    return timer;
+  }
+
+  public void destroyTimer() {
+    this.timer = null;
+    this.timerTask = null;
+  }
+
 
   @Override
   public VoxelShape getShape() {
@@ -38,26 +67,40 @@ public class WizardBlock extends SimplePlushable {
   @Override
   public ActionResult onUse(BlockState state, World world, BlockPos pos,
                             PlayerEntity player, Hand hand, BlockHitResult hit) {
+
+    if (timer == null) this.timer = registerTimer(world, pos, state);
     // Injects superclass method
     super.onUse(state, world, pos, player, hand, hit);
     if (player.shouldCancelInteraction()) return ActionResult.PASS;
     if (!player.isSneaking()) {
-      if (world instanceof ServerWorld serverWorld) {
-        SoundPacketHandler.sendPlayerPacketToClients(serverWorld, new SoundPacketHandler.PlayerSoundPacket(player, pos, SoundRegistry.SWMG, 1f));
-        ParticlePacketHandler.sendPacketToClients(serverWorld, new ParticlePacketHandler.ParticlePacket
-            (player, pos, "minecraft:note", 1, new Vec3d(0, 0.5, 0), 0f));
-        ParticlePacketHandler.sendPacketToClients(serverWorld, new ParticlePacketHandler.ParticlePacket
-            (player, pos, "minecraft:glow", 5, new Vec3d(0, 0, 0), 0.05f));
+      /* Only play interaction sounds and animations if block isn't on cooldown */
+      if (!world.getBlockState(pos).get(ON_COOLDOWN)) {
+        world.setBlockState(pos, state.with(ON_COOLDOWN, true));
+        /* Run Cooldown Timer */
 
-        return ActionResult.CONSUME;
+        timer.schedule(this.timerTask, 3000);
+        if (world instanceof ServerWorld serverWorld) {
+          SoundPacketHandler.sendPlayerPacketToClients(serverWorld, new SoundPacketHandler.PlayerSoundPacket(player, pos, SoundRegistry.SWMG, 1f));
+          ParticlePacketHandler.sendPacketToClients(serverWorld, new ParticlePacketHandler.ParticlePacket
+              (player, pos, "minecraft:note", 1, new Vec3d(0, 0.5, 0), 0f));
+          ParticlePacketHandler.sendPacketToClients(serverWorld, new ParticlePacketHandler.ParticlePacket
+              (player, pos, "minecraft:glow", 5, new Vec3d(0, 0, 0), 0.05f));
 
-      } else {
-        PlushablesNetworking.playSoundOnClient(SoundRegistry.SWMG, world, pos, 1f, 1f);
-        PlushablesNetworking.spawnParticlesOnClient(ParticleTypes.NOTE, world, pos, 1, new Vec3d(0, 0.5, 0), 0);
-        PlushablesNetworking.spawnParticlesOnClient(ParticleTypes.GLOW, world, pos, 5, new Vec3d(0, 0, 0), 0.05f);
-        return ActionResult.SUCCESS;
+          return ActionResult.CONSUME;
+
+        } else {
+          PlushablesNetworking.playSoundOnClient(SoundRegistry.SWMG, world, pos, 1f, 1f);
+          PlushablesNetworking.spawnParticlesOnClient(ParticleTypes.NOTE, world, pos, 1, new Vec3d(0, 0.5, 0), 0);
+          PlushablesNetworking.spawnParticlesOnClient(ParticleTypes.GLOW, world, pos, 5, new Vec3d(0, 0, 0), 0.05f);
+          return ActionResult.SUCCESS;
+        }
       }
     }
     return ActionResult.PASS;
+  }
+
+  @Override
+  protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+    builder.add(ON_COOLDOWN).add(FACING);
   }
 }
