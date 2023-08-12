8f7b25920a53e4421ec9a4daeeab0ad7a276e04a
Khazoda <khazoda@gmail.com>
2023-08-12 23:43:44 +0100

    config implementation base functional

--- src/main/java/com/seacroak/plushables/config/PlushablesConfig.java
+++ src/main/java/com/seacroak/plushables/config/PlushablesConfig.java
@@ -7,10 +7,12 @@ public class PlushablesConfig extends MidnightConfig {
   @Comment(centered = true) public static Comment title;
 
   @Comment public static Comment betaWarning;
-  @Entry public static boolean enableBasket = true;
   @Comment public static Comment spacer;
+  @Entry public static boolean enable_baskets = true;
+  @Comment public static Comment spacer1;
   @Comment public static Comment experimentalWarning;
-  @Entry public static boolean allowAllBlockItemsInBasket = false;
+  @Comment public static Comment spacer2;
+  @Entry public static boolean allow_all_block_items_in_baskets = false;
 
 
 }
