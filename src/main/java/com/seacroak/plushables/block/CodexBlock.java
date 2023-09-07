4aa365547045b2f2059ad9818638507f93045db5
Khazoda <khazoda@gmail.com>
2023-09-08 00:31:10 +0100

    Codex right click functionality cleaned up

--- src/main/java/com/seacroak/plushables/block/CodexBlock.java
+++ src/main/java/com/seacroak/plushables/block/CodexBlock.java
@@ -3,15 +3,24 @@ package com.seacroak.plushables.block;
 import com.seacroak.plushables.util.VoxelShapeUtils;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ItemScatterer;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class CodexBlock extends HorizontalFacingBlock {
 
@@ -20,35 +29,48 @@ public class CodexBlock extends HorizontalFacingBlock {
     setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
   }
 
-  public VoxelShape getShape() {
-    VoxelShape shape = VoxelShapes.empty();
-    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.296875, 0, 0.25, 0.765625, 0.125, 0.78125));
-    return shape;
-  }
-
-  final VoxelShape blockShape = getShape();
-  final VoxelShape[] blockShapes = VoxelShapeUtils.calculateBlockShapes(blockShape);
-
   @Override
-  public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-    Direction direction = state.get(FACING);
-    return VoxelShapeUtils.getSidedOutlineShape(direction, blockShape, blockShapes);
+  public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+    if (world instanceof ServerWorld serverWorld) {
+      ItemScatterer.spawn(world, pos, DefaultedList.ofSize(1, new ItemStack(this)));
+      world.updateComparators(pos, this);
+      world.removeBlock(pos, false);
+      return ActionResult.CONSUME;
+    } else if (world.isClient) {
+      return ActionResult.SUCCESS;
+    }
+    return ActionResult.PASS;
   }
 
-  // Render Type
-  @Override
-  public BlockRenderType getRenderType(BlockState state) {
-    return BlockRenderType.MODEL;
-  }
-  // Initial state upon placing
-  @Override
-  public BlockState getPlacementState(ItemPlacementContext context) {
-    return this.getDefaultState().with(Properties.HORIZONTAL_FACING, context.getHorizontalPlayerFacing().getOpposite());
-  }
-  // Append initial properties
-  protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-    builder.add(FACING);
-  }
+    public VoxelShape getShape () {
+      VoxelShape shape = VoxelShapes.empty();
+      shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.296875, 0, 0.25, 0.765625, 0.125, 0.78125));
+      return shape;
+    }
+
+    final VoxelShape blockShape = getShape();
+    final VoxelShape[] blockShapes = VoxelShapeUtils.calculateBlockShapes(blockShape);
+
+    @Override
+    public VoxelShape getOutlineShape (BlockState state, BlockView world, BlockPos pos, ShapeContext context){
+      Direction direction = state.get(FACING);
+      return VoxelShapeUtils.getSidedOutlineShape(direction, blockShape, blockShapes);
+    }
+
+    // Render Type
+    @Override
+    public BlockRenderType getRenderType (BlockState state){
+      return BlockRenderType.MODEL;
+    }
+    // Initial state upon placing
+    @Override
+    public BlockState getPlacementState (ItemPlacementContext context){
+      return this.getDefaultState().with(Properties.HORIZONTAL_FACING, context.getHorizontalPlayerFacing().getOpposite());
+    }
+    // Append initial properties
+    protected void appendProperties (StateManager.Builder < Block, BlockState > builder){
+      builder.add(FACING);
+    }
 
 
-}
+  }
--- src/main/java/com/seacroak/plushables/item/CodexItem.java
+++ src/main/java/com/seacroak/plushables/item/CodexItem.java
@@ -5,7 +5,9 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.TypedActionResult;
@@ -16,6 +18,20 @@ public class CodexItem extends BlockItem {
 
   public CodexItem(Block block, FabricItemSettings settings) {
     super(block, settings);
+  }
+
+  @Override
+  public ActionResult useOnBlock(ItemUsageContext context) {
+    PlayerEntity player = context.getPlayer();
+    if (!player.isSneaking()) {
+      if (player instanceof ServerPlayerEntity sp) {
+        PatchouliAPI.get().openBookGUI(sp, new Identifier("plushables:codex"));
+        return ActionResult.SUCCESS;
+      }
+    } else {
+      return super.useOnBlock(context);
+    }
+    return ActionResult.PASS;
   }
 
   @Override
