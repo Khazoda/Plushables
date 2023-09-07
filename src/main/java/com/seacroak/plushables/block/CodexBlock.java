71b943bbc73a5412314dd3201e9c337bfec2437c
Khazoda <khazoda@gmail.com>
2023-09-08 00:07:17 +0100

    Added codex voxelshape, tag

--- src/main/java/com/seacroak/plushables/block/CodexBlock.java
+++ src/main/java/com/seacroak/plushables/block/CodexBlock.java
@@ -1,17 +1,17 @@
 package com.seacroak.plushables.block;
 
+import com.seacroak.plushables.util.VoxelShapeUtils;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockRenderType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.*;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class CodexBlock extends HorizontalFacingBlock {
 
@@ -20,6 +20,20 @@ public class CodexBlock extends HorizontalFacingBlock {
     setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
   }
 
+  public VoxelShape getShape() {
+    VoxelShape shape = VoxelShapes.empty();
+    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.296875, 0, 0.25, 0.765625, 0.125, 0.78125));
+    return shape;
+  }
+
+  final VoxelShape blockShape = getShape();
+  final VoxelShape[] blockShapes = VoxelShapeUtils.calculateBlockShapes(blockShape);
+
+  @Override
+  public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+    Direction direction = state.get(FACING);
+    return VoxelShapeUtils.getSidedOutlineShape(direction, blockShape, blockShapes);
+  }
 
   // Render Type
   @Override
