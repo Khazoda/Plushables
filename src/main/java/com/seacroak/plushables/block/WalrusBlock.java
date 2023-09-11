00c609bbdc95a97a75f723302b7503b9a0943ebd
Khazoda <khazoda@gmail.com>
2023-09-11 16:45:56 +0100

    fixed tater models, textures. fixes walrus, ding sound

--- src/main/java/com/seacroak/plushables/block/WalrusBlock.java
+++ src/main/java/com/seacroak/plushables/block/WalrusBlock.java
@@ -9,9 +9,10 @@ public class WalrusBlock extends BasePlushable {
   @Override
   public VoxelShape getShape() {
     VoxelShape shape = VoxelShapes.empty();
-    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.25, 0, 0.0625, 0.75, 0.46875, 0.75));
-    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.296875, 0.46875, 0.0625, 0.703125, 0.78125, 0.390625));
-    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.296875, 0.46875, 0.390625, 0.703125, 0.59375, 0.5625));
+    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.315625, 0.0625, 0.40625, 0.684375, 0.46875, 0.71875));
+    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.30625, 0.40625, 0.0625, 0.69375, 0.78125, 0.375));
+    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.40625, 0.46875, 0.03125, 0.59375, 0.65625, 0.21875));
+    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.309375, 0.125, 0.15625, 0.690625, 0.59375, 0.53125));
     return shape;
   }
 }
