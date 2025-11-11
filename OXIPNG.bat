d1d4407c658a0acef00e6a83ee003508147b088d
khazoda <khazoda@gmail.com>
2025-11-11 19:11:19 +0000

    unhardcoded oxipng path

--- OXIPNG.bat
+++ OXIPNG.bat
@@ -3,9 +3,10 @@ echo Processing mod resources...
 echo.
 
 :: Process PNG files in resources folder and all subfolders
+:: Link to oxipng root in environment variables of your system
 for /r ".\common\src\main\resources" %%f in (*.png) do (
     echo Processing: "%%f"
-    "C:\Users\USER\Creative\OxiPNG\oxipng" -o max --alpha --strip all -Z "%%f"
+    oxipng -o max --alpha --strip all -Z "%%f"
 )
 
 PAUSE
